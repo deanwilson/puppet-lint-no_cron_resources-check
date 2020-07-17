31a0783bac354839c72fce713af68e7223235a04
Dean Wilson <dean.wilson@gmail.com>
2020-07-17 09:00:47 +0100

    Fix rubop issues

--- lib/puppet-lint/plugins/no_cron_resources.rb
+++ lib/puppet-lint/plugins/no_cron_resources.rb
@@ -1,17 +1,15 @@
 PuppetLint.new_check(:no_cron_resources) do
-
   def check
     resource_indexes.each do |resource|
       if resource[:type].value == 'cron'
 
         notify :warning, {
-          :message => 'cron resources should not be used',
-          :line    => resource[:type].line,
-          :column  => resource[:type].column,
+          message: 'cron resources should not be used',
+          line:    resource[:type].line,
+          column:  resource[:type].column,
         }
 
       end
     end
   end
-
 end
